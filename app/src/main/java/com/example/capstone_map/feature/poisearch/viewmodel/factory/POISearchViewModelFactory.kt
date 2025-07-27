3b736a517b8793c5ed90ffcfaccf2188a6e29bee
leesu9042 <tnckddl8957@gmail.com>
2025-07-27 17:30:52 +0900

    Replace app with Capstone_Map/refactor/mvvm version

--- app/src/main/java/com/example/capstone_map/feature/poisearch/viewmodel/factory/POISearchViewModelFactory.kt
+++ app/src/main/java/com/example/capstone_map/feature/poisearch/viewmodel/factory/POISearchViewModelFactory.kt
@@ -3,18 +3,20 @@ package com.example.capstone_map.feature.poisearch.viewmodel.factory
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.capstone_map.feature.poisearch.viewmodel.POISearchViewModel
-import com.example.capstone_map.common.location.LocationFetcher
+import com.example.capstone_map.common.location.oncefetcher.LocationFetcher
 import com.example.capstone_map.common.viewmodel.NavigationStateViewModel
 import com.example.capstone_map.common.voice.STTManager
 import com.example.capstone_map.common.voice.TTSManager
+import com.example.capstone_map.feature.navigation.viewmodel.NavigationViewModel
 
 class POISearchViewModelFactory(
     private val stateViewModel: NavigationStateViewModel,
+    private val navigationViewModel : NavigationViewModel,
     private val locationFetcher: LocationFetcher,
     private val ttsManager: TTSManager,
     private val sttManager: STTManager
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return POISearchViewModel(stateViewModel,locationFetcher,ttsManager,sttManager) as T
+        return POISearchViewModel(stateViewModel,navigationViewModel,locationFetcher,ttsManager,sttManager) as T
     }
 }
