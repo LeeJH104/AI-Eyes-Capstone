c7c0bca645c08a5d7e0b71b8472d7cd4a74844c3
LeeJH104 <dlwogns104@gmail.com>
2025-07-27 22:18:11 +0900

    create ReceiptActivity.java

---
+++ app/src/main/java/com/example/aieyes/ui/ReceiptActivity.java
@@ -0,0 +1,15 @@
+package com.example.aieyes.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aieyes.R;
+
+public class ReceiptActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.receipt_activity);
+    }
+}
