38950f108fdddf500c41edcba0cb05148bc7472b
jodoo1 <jodoo20@gmail.com>
2025-09-05 16:54:07 +0900

    필요파일 옮기기, 레시피앱수정

--- backend/receipt_app.py
+++ backend/receipt_app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from receipt.routes import receipt_bp
+from obstacle.obst_routes import obstacle_bp
 
 app = Flask(__name__)
-app.register_blueprint(receipt_bp, url_prefix='/api')
+# 영수증 분석 API를 /api/receipt 경로에 등록
+app.register_blueprint(receipt_bp, url_prefix='/api/receipt')
+
+# 장애물 분석 API를 /api/obstacle 경로에 등록
+app.register_blueprint(obstacle_bp, url_prefix='/api/obstacle')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
