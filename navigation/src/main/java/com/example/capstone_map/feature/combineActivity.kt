c37cf52a22107b63ab47febe7a39a3b299d24dbb
HyunHoCom <skfnxh7637@naver.com>
2025-10-25 15:57:09 +0900

    integration

--- navigation/src/main/java/com/example/capstone_map/feature/combineActivity.kt
+++ navigation/src/main/java/com/example/capstone_map/feature/combineActivity.kt
@@ -1,27 +1,24 @@
 package com.example.capstone_map.feature
 
-
-
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.capstone_map.R
-
-
-// import com.example.aieyes.feature.CameraFragment  // ✅ CameraFragment import
+import com.example.capstone_map.feature.ObstacleDetectionFragment
 
 class CombinedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_combined)
 
-//        // ✅ 카메라 Fragment 추가 (상단)
-//        supportFragmentManager.beginTransaction()
-//            .replace(R.id.camera_container, CameraFragment())
-//            .commit()
+        if (savedInstanceState == null) {
 
-        // ✅ 맵 Fragment 추가 (하단)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.map_container, MapFragment())
-            .commit()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.camera_container, ObstacleDetectionFragment()) // ✅ CameraFragment -> ObstacleDetectionFragment
+                .commit()
+
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.map_container, MapFragment())
+                .commit()
+        }
     }
 }
